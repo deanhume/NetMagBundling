f197f15af2faf72ea9aa9264468639b6de753398
Dean <contact@deanhume.com>
2013-04-11 06:15:08 +0100

    Added initial files

---
+++ NetMagBundling/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace NetMagBundling
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Minify the CSS
+            bundles.Add(new StyleBundle("~/css/minify").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/bootstrap-responsive.css"));
+        
+            // Minify the JavaScript
+            bundles.Add(new ScriptBundle("~/js/minify").Include(
+                "~/Scripts/bootstrap.js"));
+        }
+    }
+}
